ff799208a79257740fac5b53c3e9b5b9ef30b968
martyall <martyall@protonmail.com>
2023-12-01 16:52:27 -0800

    wip

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
       );
 
   nixConfig = {
-    extra-substituters = ["https://cache.nixos.org", "hydra.iohk.io"];
+    extra-substituters = ["https://cache.nixos.org"];
     extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
     allow-import-from-derivation = true;
   };
