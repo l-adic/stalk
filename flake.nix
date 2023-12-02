b67fbbe6a484b98028f0cbe92b183dcacc3bd8de
martyall <martyall@protonmail.com>
2023-12-01 16:25:54 -0800

    wip

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+      ];
+    in
+      flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        overlays =
+          [ haskellNix.overlay
+              (final: prev: {
+                hixProject =
+                  final.haskell-nix.project {
+                    src = ./.;
+                    compiler-nix-name = "ghc901";
+                    evalSystem = "x86_64-linux";
+                  };
+                }
+              )
+          ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.hixProject.flake {};
+      in flake // rec
+           { legacyPackages = pkgs;
+             packages =  
+               { foo = flake.packages."straw:lib:straw";
+                 all = pkgs.symlinkJoin {
+                   name = "all";
+                   paths = with packages;
+                     [ straw
+                     ];
+                 };
+                 default = packages.all;
+               };
+             devShells =
+               { default =
+                  pkgs.hixProject.shellFor {
+                    tools = {
+                      cabal = "3.6.0.0"; # this is the version specified in all of the *.cabal files.
+                      haskell-language-server = "latest";
+                    };
+                  };
+               };
+           }
+      );
+
+  nixConfig = {
+    extra-substituters = ["https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    allow-import-from-derivation = true;
+  };
+}
