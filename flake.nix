1549c2946130e927afb77905c44593c5374fbbeb
martyall <martyall@protonmail.com>
2023-12-01 16:38:49 -0800

    wip

--- flake.nix
+++ flake.nix
@@ -39,8 +39,8 @@
       );
 
   nixConfig = {
-    extra-substituters = ["https://cache.iog.io"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    extra-substituters = ["https://cache.nixos.org" "https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
     allow-import-from-derivation = true;
   };
 }
