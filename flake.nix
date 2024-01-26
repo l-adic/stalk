c620ea4147968a9c99214294cfbb04713442ac21
martyall <martyall@protonmail.com>
2024-01-25 20:01:43 -0800

    builds

--- flake.nix
+++ flake.nix
@@ -1,50 +1,35 @@
 {
-  description = "A type for integers modulo some constant.";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        haskellPackages = pkgs.haskellPackages;
+        hpkgs = pkgs.haskell.packages.ghc8107;
 
-        package = ghc:
-          pkgs.haskell.packages."${ghc}".developPackage {
-            name = "stalk";
-            root = ./.;
+      in {
+        formatter = pkgs.nixfmt;
 
-            overrides = self: super: with pkgs.haskell.lib; { };
+        devShells = let
+          ## The minimal dependency set to build the project with `cabal`.
+          buildInputs = ([ hpkgs.ghc ]) ++ (with pkgs; [
+            cabal-install
+            zlib
+          ]);
+
+        in {
+          ci = pkgs.mkShell {
+            inherit buildInputs;
           };
 
-        shellFor = ghc:
-          pkgs.haskell.packages."${ghc}".shellFor {
-            packages = p: [ (package ghc)  ];
-
-            buildInputs = with haskellPackages; [
-              cabal-install
-              ghcid
-              haskell-language-server
-
-              pkgs.haskellPackages.cabal-fmt
-              pkgs.nixpkgs-fmt
-            ];
+          default = pkgs.mkShell {
+            ## NOTE: `pkgs.ormolu` must appear before `hpkgs.haskell-language-server`
+            ## in the `buildInputs`, so as to take precedence. This ensures that the
+            ## version of Ormolu available in the path is that of nixpkgs and not the
+            ## one pinned by HLS.
+            buildInputs = buildInputs ++ (with pkgs; [ ormolu hpack hlint ]);
           };
-      in
-      rec {
-        # TODO: cleaner way to manage multiple GHC versions...
-        packages = {
-          stalk_8107 = package "ghc8107";
         };
-
-        devShells = {
-          stalk_8107 = shellFor "ghc8107";
-        };
-
-        defaultPackage = packages.stalk_8107;
-        devShell = devShells.stalk_8107;
       });
 }
